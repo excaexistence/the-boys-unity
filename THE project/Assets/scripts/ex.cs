a14c9d73b41707be648afe82d9b3355dbb6935bd
Davos <excathedragames@gmail.com>
2024-03-10 13:19:09 +0100

    code updt

--- THE project/Assets/scripts/ex.cs
+++ THE project/Assets/scripts/ex.cs
@@ -6,76 +6,63 @@ using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 
-public class ex : MonoBehaviour
-{
-    public GameObject obj; string infoname; bool onscreen; float[] data; string currdata; string[] currarrdata; int maxlogsize = 10; Queue<string> logQ = new Queue<string>();
-    public GameObject player; System.Diagnostics.StackTrace t = new System.Diagnostics.StackTrace(); 
-    
+public class ex : MonoBehaviour {
+    public GameObject obj; string infoname; bool onscreen; string currdata; string[] dataname; int maxlogsize = 10; Queue<string> logQ = new Queue<string>();
+    public GameObject player; System.Diagnostics.StackTrace t = new System.Diagnostics.StackTrace();
 
     void Awake() {
-        player = new GameObject();
+        player = new GameObject("Player"); 
+        infoname = "Player Position : ";
     }
-    void Start()
-    {
-        onscreen = false;
-        player.name = "Player";
-        player.SetActive(true);
+    void Start() {
+        //onscreen = false;
         player.tag = "Player";
+        player.SetActive(true);
+        //logQ.Enqueue(" > " + infoname);
         //add if input º onscreen true
         //todo
-
-        currdata = "Log started";
     }
-    void Update()
-    {
-        /*float playerX = player.transform.position.x;
-        float playerY = player.transform.position.y;
-        currarrdata[0] = player.name;
-        currarrdata[1] = "X : " + playerX.ToString();
-        currarrdata[2] = "Y : " + playerY.ToString();
-        for(int e = 0; e < currarrdata.Length; e++){
-            currdata += " " + currarrdata[e];
-        }
-        //string h = "hello world";
-        //Log(currdata,h,LogType.Log);
-        /*if(data.Length > 0)Array.Clear(data,0,data.Length);
-        else {
-            Log(objname,obj,onscreen,data);
-        }*/
-        float px = player.transform.position.x;
-        currdata = "player X : " + px.ToString();
+    void Update() {
+        float px = player.transform.position.x; float py = player.transform.position.y;
+        string thisdata = infoname + "X : " + px.ToString() + ", " + "Y : " + py.ToString();
+        Log(thisdata,t.ToString(),LogType.Log);
     }
-    void OnEnable()
-    {
+    void OnEnable() {
         Application.logMessageReceivedThreaded += Log;
     }
 
-    void OnDisable()
-    {
+    void OnDisable() {
         Application.logMessageReceivedThreaded -= Log;
     }
 
     void Log(string currdata, string stackTrace, LogType type) {
-        if (logQ.Count >= maxlogsize)logQ.Dequeue();
-        logQ.Enqueue(currdata);
-        //if(onscreen == true)currdata = (infoname + obj + data[i]).ToString();
+        logQ.Enqueue("[" + type + "] : " + currdata);
+        if(type == LogType.Exception)logQ.Enqueue(stackTrace);
+        while(logQ.Count > maxlogsize)logQ.Dequeue();
         //log info on console ind. if onscreen
         Debug.Log(currdata);
     }
     void OnGUI() {
         GUI.Label(
            new Rect(
-               5,                   //x
-               5,                   //y
-               Screen.width - 5,    //width
+               0,                   //x
+               0,                   //y
+               Screen.width,        //width
                Screen.height/4      //height
            ),      
-           currdata                //text
+           currdata,                //text
+           GUI.skin.textArea
         );
+        GUILayout.Label(string.Join("\n", logQ.ToArray()));
     }
-
-    public float Valor(float x,float y) {
-        
-        return 0;
+    //todo unfinished make push function to add elements to an array
+    string[] Push(string[] source,float[] cvalues) {
+        var index = Array.IndexOf(source, cvalues);
+        for(int i = 0; i < cvalues.Length; i++){
+            if(index != -1) {
+                source[index] = cvalues[i].ToString();
+            }
+        }
+        return source;
     }
 }
